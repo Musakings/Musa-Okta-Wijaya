9da03009cc53973382cf3bec780b4b6eb635bb3f
Musakings <xdrake618.@gmail.com>
2024-08-30 13:50:10 +0700

    musa okta wijaya

---
+++ Prak Big Data/Prak PBO/proyek 1/Musa_project1/src/main/java/com/mycompany/musa_project1/Musa_project1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.musa_project1;
+
+/**
+ *
+ * @author Musa Okta
+ */
+public class Musa_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Musa Okta Wijaya");
+        System.out.println("NPM     : 2340506060");
+        System.out.println("Alamat  : Banjaran, Tempurejo, Tempuran, Magelang");
+        System.out.println("No. Hp  : 087853866487");
+    }
+}
+
+    
+
+
+
