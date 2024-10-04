def3e17ec43f7f6af89d53338eaaa58e88c7dd61
Musakings <xdrake618.@gmail.com>
2024-10-04 18:49:41 +0700

    Inheritance-Pewarisan

---
+++ Prak PBO/Praktikum_PBO5/src/main/java/praktikum5/Anjing.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum5;
+
+/**
+ *
+ * @author Musza
+ */
+public class Anjing extends Hewan {
+
+    @Override
+    public void TampilkanInfo() {
+        super.TampilkanInfo();
+        System.out.println("Suara: Gug Gug");
+        System.out.println("Ini Anjing");
+    }
+}
---
+++ Prak PBO/Praktikum_PBO5/src/main/java/praktikum5/Kucing.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum5;
+
+/**
+ *
+ * @author Musza
+ */
+public class Kucing extends Hewan {
+    @Override
+    public void TampilkanInfo() {
+        super.TampilkanInfo();
+        System.out.println("Suara: Meow");
+        System.out.println("Ini Kucing");
+    }
+}
