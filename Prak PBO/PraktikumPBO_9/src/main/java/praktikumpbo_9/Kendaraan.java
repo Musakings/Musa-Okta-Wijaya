83e50b4869da237ea96669ffffa8de449a045680
Musakings <xdrake618.@gmail.com>
2024-11-16 15:58:46 +0700

    Abstraksi dan Kelas Abstrak

---
+++ Prak PBO/PraktikumPBO_9/src/main/java/praktikumpbo_9/Kendaraan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikumpbo_9;
+
+/**
+ *
+ * @author Musza
+ */
+abstract class Kendaraan {
+    abstract void berjalan();
+    void info() {
+        System.out.println("ini adalah kendaraan");
+    }
+}
